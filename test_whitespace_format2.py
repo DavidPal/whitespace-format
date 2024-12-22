d8d733032afbae0282bcf9cdc68c01b975bda73f
David Pal <davidko.pal@gmail.com>
2024-12-22 21:56:42 +0100

    Add unit test

--- test_whitespace_format2.py
+++ test_whitespace_format2.py
@@ -830,6 +830,34 @@ class TestWhitespaceFormat(unittest.TestCase):
             ),
         )
 
+    def test_format_file_content__remove_trailing_whitespace_and_remove_trailing_empty_lines(self):
+        """Tests format_file_content() function."""
+        self.assertEqual(
+            (
+                "hello world\n",
+                [
+                    Change(ChangeType.REMOVED_TRAILING_WHITESPACE, 1),
+                    Change(ChangeType.REMOVED_TRAILING_WHITESPACE, 3),
+                    Change(ChangeType.REMOVED_EMPTY_LINES, 2),
+                ],
+            ),
+            whitespace_format2.format_file_content(
+                "hello world   \n\n   \n",
+                argparse.Namespace(
+                    add_new_line_marker_at_end_of_file=False,
+                    new_line_marker="auto",
+                    normalize_empty_files="ignore",
+                    normalize_new_line_markers=False,
+                    normalize_non_standard_whitespace="replace",
+                    normalize_whitespace_only_files="ignore",
+                    remove_new_line_marker_from_end_of_file=False,
+                    remove_trailing_empty_lines=True,
+                    remove_trailing_whitespace=True,
+                    replace_tabs_with_spaces=-1,
+                ),
+            ),
+        )
+
 
 if __name__ == "__main__":
     unittest.main()
