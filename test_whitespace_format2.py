0ccdaa34743036ce1bedbd9ddbd13273c0f88348
David Pal <davidko.pal@gmail.com>
2024-12-22 22:05:52 +0100

    Add unit test

--- test_whitespace_format2.py
+++ test_whitespace_format2.py
@@ -858,6 +858,27 @@ class TestWhitespaceFormat(unittest.TestCase):
             ),
         )
 
+    def test_format_file_content__replace_tabs_with_spaces__ignore(self):
+        """Tests format_file_content() function."""
+        self.assertEqual(
+            ("\t", []),
+            whitespace_format2.format_file_content(
+                "\t",
+                argparse.Namespace(
+                    add_new_line_marker_at_end_of_file=False,
+                    new_line_marker="auto",
+                    normalize_empty_files="ignore",
+                    normalize_new_line_markers=False,
+                    normalize_non_standard_whitespace="replace",
+                    normalize_whitespace_only_files="ignore",
+                    remove_new_line_marker_from_end_of_file=False,
+                    remove_trailing_empty_lines=True,
+                    remove_trailing_whitespace=True,
+                    replace_tabs_with_spaces=-47,
+                ),
+            ),
+        )
+
 
 if __name__ == "__main__":
     unittest.main()
