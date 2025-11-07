039194ccb34c31a62ea1a18650013957600b2ce1
David Pal <davidko.pal@gmail.com>
2025-11-06 21:07:54 -0800

    Increase version number

--- whitespace_format.py
+++ whitespace_format.py
@@ -22,7 +22,7 @@ from enum import Enum
 from typing import List
 from typing import Tuple
 
-VERSION = "0.0.10"
+VERSION = "0.0.11"
 
 # Regular expression that does NOT match any string.
 UNMATCHABLE_REGEX = "$."
